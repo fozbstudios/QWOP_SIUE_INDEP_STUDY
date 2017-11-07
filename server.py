99d8456c63736758983a584e6b17b2332ae86582
Nick K <local>
2017-11-07 00:43:02 -0600

    refactored server into reusable class

--- server.py
+++ server.py
@@ -3,37 +3,80 @@ from flask import Flask,request, redirect, url_for, send_from_directory
 
 import time
 from flask_socketio import SocketIO
+class QWOPInputOutput:
+    def __init__(self):
+        self.app = Flask(__name__,static_folder='webfiles');
+        self.socketio = SocketIO(app)
+        self.died=False
+        self.curScore=0
+        self.tickCount=0
+        self.finScore=0
+        self.QPressed=False
+        self.WPressed=False
+        self.OPressed=False
+        self.PPressed=False
+        self.firstRun=True
 
-app = Flask(__name__,static_folder='webfiles');
-socketio = SocketIO(app)
-app.debug = True
 
+        # Routes
+        @app.route('/')
+        def root():
+            return app.send_static_file('index.html')
 
-# Routes
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
+        @app.route('/<path:path>')
+        def static_proxy(path):
+            # send_static_file will guess the correct MIME type
+          return app.send_static_file(path)
 
-@app.route('/<path:path>')
-def static_proxy(path):
-    # send_static_file will guess the correct MIME type
-  return app.send_static_file(path)
+        @socketio.on('connect')
+        def hc():
+            print('connect\n\n\n')
+        #@socketio.on('current score')
+        #def hc(cs):
+            #print(repr(cs) +'\n\n\n')
+        @socketio.on('final score')
+        def hc(self, fs):
+            #print(repr(cs) +'\n\n\n')
+            self.finScore=fs
+            self.QPressed=False
+            self.WPressed=False
+            self.OPressed=False
+            self.PPressed=False
+            self.curScore=0
+            self.tickCount=0
+            self.died=True
+        @socketio.on('current score')
+        def hc(self, cs):
+            self.curScore=cs
+            self.tickCount+=1
 
-@socketio.on('connect')
-def hc():
-    print('connect\n\n\n')
-#@socketio.on('current score')
-#def hc(cs):
-    #print(repr(cs) +'\n\n\n')
-@socketio.on('final score')
-def hc(cs):
-    print(repr(cs) +'\n\n\n')
-    
-@socketio.on('serverReady')
-def hc():
-    print('clicking\n\n\n')
-    socketio.emit('aiReady')
-    socketio.emit('pressP')
+        @socketio.on('serverReady')
+        def hc(self):
+            # print('clicking\n\n\n')
+            self.died=False;
+            if self.firstRun==True:
+                socketio.emit('aiReady')
+                self.firstRun=False
+    def controlManage(self, eventStr):
+        if eventStr=="pQ":
+            self.QPressed=True;socketio.emit('pressQ')
+        elif eventStr=="pW":
+            self.WPressed=True;socketio.emit('pressW')
+        elif eventStr=="pO":
+            self.OPressed=True;socketio.emit('pressO')
+        elif eventStr=="pP":
+            self.PPressed=True;socketio.emit('pressP')
+        elif eventStr=="rQ":
+            self.QPressed=False;socketio.emit('releaseQ')
+        elif eventStr=="rW":
+            self.WPressed=False;socketio.emit('releaseW')
+        elif eventStr=="rO":
+            self.OPressed=False;socketio.emit('releaseO')
+        elif eventStr=="rP":
+            self.PPressed=False;socketio.emit('releaseP')
+        else:# bad arg
+            errStr="eventStr %s is undefined!" % eventStr
+            raise NameError(errStr)
 if __name__ == '__main__':
+
     socketio.run(app)
-    
